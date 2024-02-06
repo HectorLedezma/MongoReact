260e6c5819e790151e121536bb2e579b993c9540
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-02-06 14:33:40 -0300

    archivos de cache

--- react-app/src/Components/Parametros.jsx
+++ react-app/src/Components/Parametros.jsx
@@ -1,26 +1,35 @@
-//import { useState } from "react";
-
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 
 function Paramets(props){
     
-    const [tags,setTags] = useState([<div/>])
+    const [tags,setTags] = useState([])
+    const tagRef = useRef();
+    const [intereses,setIntereses] = useState(["ciencia","tecnología","salud","social","psicología","política","recreación","innovación","negocios","deporte"]);
+    
+    const [listInt,setInte] = useState(false);
+    const togInteres = () =>{
+        setInte(!listInt);
+    }
 
     const inteList = () =>{
-        const intereses = ["ciencia","tecnología","salud","social","psicología","política","recreación","innovación","negocios","deporte"];
         let Ilist = [];
         for(let i = 0; i<intereses.length;i++){
             Ilist.push(
                 <li 
                     className="dropdown-item"
                     role="button"
+                    key={"liInter_"+i}
                     onClick={
                         ev=>{
                             ev.preventDefault();
-                            
-                            togInteres();
-                            setTags(tags.concat([<label className="bg-info me-3 mt-3 p-1 border rounded">{intereses[i]}</label>]))
+                            //setInte(!listInt);
+                            setTags(tags.concat(
+                                [<label key={"tagInter_"+i} className="bg-info ms-3 p-1 border rounded">{intereses[i]}</label>]
+                            ))
+                            setIntereses(intereses.filter(interes => interes !== intereses[i]));
+                            console.log(intereses)
+                            setList(inteList())
                         }
                     }
                 >
@@ -30,11 +39,9 @@ function Paramets(props){
         }
         return Ilist;
     }
+    const [list,setList] = useState(inteList())
+
     
-    const [listInt,setInte] = useState(false);
-    const togInteres = () =>{
-        setInte(!listInt);
-    }
 
 
     switch (props.type) {
@@ -53,17 +60,26 @@ function Paramets(props){
                         <label style={{width:"50%"}}>{'RUT: '}</label>
                         <input className="input" type="text" style={{width:"50%"}}/>
                     </div>
-                    <div className="mb-2">
+                    <div className="mb-2 d-flex text-center">
                         <div className="dropdown">
-                            <button onClick={togInteres} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="true">
+                            <button onClick={
+                                ev=>{
+                                    ev.preventDefault();
+                                    togInteres();
+                                    //console.log(intereses)
+                                    setList(inteList())
+                                }
+
+                            } className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="true">
                                 Intereses
                             </button>
-                            <ul className={`dropdown-menu dropdown-menu-dark ${listInt ? 'show' : ''}`}>
-                                {inteList()}
+                            <ul key="userInter" className={`dropdown-menu dropdown-menu-dark ${listInt ? 'show' : ''}`}>
+                                {list}
                             </ul>
                         </div>
-                        <div>{tags}</div>
+                        <div className="align-self-center" ref={tagRef}>{tags}</div>
                     </div>
+                    <button type="button" className="btn btn-primary">Buscar</button>
                 </form>
             );
         case 2://Salas
