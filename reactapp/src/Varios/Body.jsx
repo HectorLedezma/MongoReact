d73aec3556c9fba6b08db9a8c5b1563b28c8d838
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-11-07 02:53:51 -0300

    re ubicacion del cuerpo de la pagina

---
+++ reactapp/src/Varios/Body.jsx
@@ -0,0 +1,54 @@
+import Cookies from "universal-cookie";
+import Login from "../Pages/Login";
+//import { useNavigate } from "react-router-dom";
+import { Connection } from "../Conection/connection";
+//import cryptoJs from "crypto-js";
+//import { Rut } from "../Varios/TemaRut";
+import Profile from "../Components/Profile";
+import Salas from "../Pages/salas";
+import { useEffect, useState } from "react";
+import Default from "../Pages/default";
+
+function Body(props){
+    //const gestRut = new Rut();
+    const [log,setLog] = useState(false);
+    
+    useEffect(()=>{
+        const con = new Connection();
+        con.log(cookie.get('UserToken')).then(loged =>{
+           // console.log('logueado: (body)',loged);
+            setLog(loged)
+        }).catch(()=>{
+            console.log("no hay token")
+        });
+    })
+    const cookie = new Cookies();
+    //const navi = useNavigate();
+    switch (props.route) {
+        case '/salas':
+            return(//se puede mostrar la vista de las salas
+                <Salas/>
+            )
+        case '/user':
+            if(log){
+                return(//perfil de usuario
+                    <Profile/>
+                )
+            }else{
+                return(//ingreso de usuario
+                    <Login/>
+                )
+            }
+            
+        case '/login':
+            return(//ingreso de usuario
+                <Login/>
+            )
+        default:
+            return(
+                <Default/>
+            )
+    }
+}
+
+export default Body;
