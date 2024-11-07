83de69b4797f7adf689a7324c28766ec069fbd72
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-11-07 02:51:48 -0300

    pagina para mostrar al inicio

---
+++ reactapp/src/Pages/default.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { Connection } from "../Conection/connection";
+import '../Styles/colors.css';
+
+function Default(){
+
+    const [data,SetData] = useState([<h1 key="0">Cargando</h1>]);
+    const [loaded,SetLoad] = useState(false);
+
+    useEffect(()=>{
+        if(!loaded){
+            EnsamblaData();
+        }
+    });
+
+    const EnsamblaData = () =>{
+        const con = new Connection();
+
+        const nombre_comun = (lista) =>{
+            const i = Math.floor(Math.random() * lista.length)
+            return (
+                <h3 className="me-3 ms-3 mt-2 mb-2" key={lista[i]}>
+                    {lista[i]}
+                </h3>
+            );
+        }
+
+        con.resumenPlant().then(result=>{
+            let list = [];
+            result.forEach(d=>{
+                list.push(
+                    <div className="plant-card border border-3 rounded text-light m-3" key={d._id}>
+                        <div className="d-flex justify-content-center">
+                            {nombre_comun(d.nombre_comun)}
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <h5 className="me-3 ms-3">
+                                {d.nombre_cientifico}
+                            </h5>
+                        </div>
+                    </div>
+                )
+            })
+            //return list;
+            SetData(list);
+            SetLoad(true);
+        }).catch(e=>{
+            console.log(e);
+            SetData([<h1 key="1">Hubo un Problema al cargar los datos</h1>]);
+        });
+
+    }
+
+    return(
+        <div className="d-flex flex-wrap justify-content-evenly">
+            {data}
+        </div>
+    )
+}
+
+export default Default;
+
